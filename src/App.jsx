685513eb7a7e8566a13ce7ba27be33e47a108142
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-02-07 19:04:37 -0500

    2nd run/more styling custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { useState, useReducer} from 'react';
 import "./styles.css";
 
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  CLEAR: 'clear',
+}
+
+function reducer(state, action){
+  switch (action.type){
+    case ACTIONS.INCREMENT: 
+      return {count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count - 1}
+    case  ACTIONS.CLEAR:
+      return {count: 0}
+    default:
+      return state;
+
+  }
+}
+
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [state, dispatch ] = useReducer(reducer,{count: 0})
+  function Increment(){
+    dispatch({type: ACTIONS.INCREMENT});
+  }
 
+  function Decrement(){
+    dispatch({type: ACTIONS.DECREMENT})
+  }
+
+  function Clear(){
+    dispatch({type: ACTIONS.CLEAR})
+  }
   return (
-    <div className="counter">
-      <button onClick={() =>{setCount(count - 1)}}>-</button>
-      <span>{count}</span>
-      <button onClick={() => {setCount(count + 1)}}>+</button>
+    <div className='background'>
+      <div className="counter">
+        <h2>{state.count}</h2>
+        <button onClick={Increment}> Increment +</button>
+        <button onClick={Decrement}>Decrement -</button>
+        <button onClick={Clear}>Clear</button>
+      </div>
     </div>
+
   )
 }
 
