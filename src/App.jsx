d57ad104859da90c47603264287f052bd4c0ac21
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-03-04 20:43:06 -0500

    finished advanced reducer logic/keep for review and refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useReducer} from 'react';
 import "./styles.css";
+import Todo from"./Todo.jsx";
 
-const ACTIONS = {
-  ADD_TODO: 'add-todo'
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
  
 }
 
@@ -10,8 +13,18 @@ function reducer(todos, action){
   switch (action.type){
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)]
-  }
- 
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id){
+          return { ...todo, complete: !todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
+    }
 }
 
 function newTodo(name){
@@ -31,12 +44,17 @@ export default function App() {
   }
 
   
-  
+  console.log(todos)
   return (
     <div className="background">
       <form onSubmit={handleSubmit}>
         <input type="text" value={name} onChange={e => setName(e.target.value)}/>
       </form>
+      {
+        todos.map(todo => {
+          return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+        })
+      }
     </div>
 
   )
